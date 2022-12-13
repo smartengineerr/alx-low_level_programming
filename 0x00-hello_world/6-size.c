1b3ec0c5e1009e5f556a62c5e11c8ef2e00aff2a
smartengineerr <platbookez@gmail.com>
2022-12-12 19:05:27 -0800

    Phase code c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,9 @@
+i#include <stdio.h>
+main()
+{
+	printf("Size of char: %zu\n", sizeof(char));
+	printf("Size of int: %zu\n", sizeof(int));
+	printf("Size of long int: %zu\n", sizeof(long int));
+	printf("Size of long long int: %zu\n", sizeof(long long int));
+	printf("Size of float: %zu\n", sizeof(float));
+}
