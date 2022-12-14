2af90dcda00ac78f827e6797c3c15300f562be3e
smartengineerr <platbookez@gmail.com>
2022-12-13 21:52:04 -0800

    ongoing

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <main.h>
+	/*
+	 * include main.h is an addition header
+	 *
+	 * starting function
+	 */
+
+int main (void)
+	
+{
+	int a, b;
+	a = 65;
+	b = 70;
+	while (a != b);
+	a=getchar();
+	putchar(a);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+	/* Header of a function
+	 *
+	 * Main is the beginning of a function
+	 *
+	 * return is 0 (Success)
+	 */
+int print_alphabet (void)
+{
+	int char
+		char = letter;
+		for (letter = '1'; letter <='20'; letter++);
+		_putchar(letter);
+
+	_putchar('\n');
+
+}
